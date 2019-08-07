43b695691ea2b81e26c759cda40a2311db320247
Dat Ngo <dat.ngo@nfq.asia>
2019-08-07 13:48:02 +0700

    rabbitMQ - add a sample for rabbitMQ with springboot2

---
+++ PracticeJavaTech/JMW_rabbitMQ/rabbitmq-example/Readme.md
@@ -0,0 +1,5 @@
+Refer to A very good and detail tutorial, based on springboot and container for RabbitMQ
+https://www.youtube.com/watch?v=yuApTF_HeWM
+
+This example is done until 10:30 from the above video.
+
---
+++ PracticeJavaTech/JMW_rabbitMQ/rabbitmq-example/src/main/java/io/tpd/jms/demo/PracticalTipListener.java
@@ -0,0 +1,17 @@
+package io.tpd.jms.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PracticalTipListener{
+    private static final Logger log = LoggerFactory.getLogger(PracticalTipListener.class);
+
+    @RabbitListener(queues = RabbitmqExampleApplication.DEFAULT_PARSING_QUEUE)
+    public void consumeDefaultMessage(final Message message) {
+        log.info("Received message with default configuration: {}", message.toString());
+    }
+}
